4699498bbc562d3817e54d780f3fafb2f97442e7
dvgba <diegovianagomes01@gmail.com>
2025-02-07 08:22:26 -0300

    BOOK2-48 added Service Token generation/validation and

--- bookworm-backend/src/main/java/com/diegoviana/bookworm_backend/infrastructure/security/SecurityConfig.java
+++ bookworm-backend/src/main/java/com/diegoviana/bookworm_backend/infrastructure/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.diegoviana.bookworm_backend.infrastructure.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +25,10 @@ public class SecurityConfig {
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
