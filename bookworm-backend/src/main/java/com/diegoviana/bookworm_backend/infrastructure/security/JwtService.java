5663bdcb73d157f46d2488e01ea292b476ce3385
dvgba <diegovianagomes01@gmail.com>
2025-02-07 08:19:41 -0300

    BOOK2-48 added Service Token generation/validation and

---
+++ bookworm-backend/src/main/java/com/diegoviana/bookworm_backend/infrastructure/security/JwtService.java
@@ -0,0 +1,41 @@
+package com.diegoviana.bookworm_backend.infrastructure.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private String secretKey;
+    public String generateToken(Object user) {
+        return Jwts.builder()
+                .setSubject(user.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    };
+}
